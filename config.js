705fcff9d91af2b023d0a5f55d4d143ff5716e3a
as9coder <135017614+as9coder@users.noreply.github.com>
2024-04-01 12:20:18 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+const firebaseConfig = {
+    apiKey: "AIzaSyBOIDZSdo4TozefJKSLuIQ037l-NORqE4o",
+    authDomain: "bicycle-f24b0.firebaseapp.com",
+    projectId: "bicycle-f24b0",
+    storageBucket: "bicycle-f24b0.appspot.com",
+    messagingSenderId: "18970019061",
+    appId: "1:18970019061:web:22bc02be2e9fd710da31bc"
+  };
+//
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
